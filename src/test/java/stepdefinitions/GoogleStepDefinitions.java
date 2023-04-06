eb30b9e0d40832d82d4d943e0157b04cb277ed97
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-04-06 11:41:23 -0500

    first

---
+++ src/test/java/stepdefinitions/GoogleStepDefinitions.java
@@ -0,0 +1,33 @@
+package stepdefinitions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+public class GoogleStepDefinitions {
+    @Given("I navigate to {string}")
+    public void i_navigate_to(String string) {
+//        open("https://www.google.com");
+        open(string);// going to URL
+        sleep(10000); //10 second
+    }
+    @Given("I wait for {int} seconds")
+    public void i_wait_for_seconds(Integer int1) {
+        sleep(int1*1000);
+        //5 seconds = 5000 milliseconds
+    }
+    @Given("I navigate back")
+    public void i_navigate_back() {
+        back();
+    }
+    @Given("I navigate forward")
+    public void i_navigate_forward() {
+        forward();
+    }
+    @Given("I refresh the page")
+    public void i_refresh_the_page() {
+        refresh();
+    }
+    @Then("I hold the browser open")
+    public void i_hold_the_browser_open() {
+    }
+}
